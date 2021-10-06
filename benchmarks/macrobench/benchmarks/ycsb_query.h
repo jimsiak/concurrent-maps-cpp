248f860f7e0a21d12769ab863e2196c6c9dd3816
Dimitrios D. Siakavaras <jimsiak@cslab.ece.ntua.gr>
2021-10-06 18:55:01 +0300

    Clean the code of YCSB macrobench.

--- benchmarks/macrobench/benchmarks/ycsb_query.h
+++ benchmarks/macrobench/benchmarks/ycsb_query.h
@@ -1,5 +1,4 @@
-#ifndef _YCSB_QUERY_H_
-#define _YCSB_QUERY_H_
+#pragma once
 
 #include "global.h"
 #include "helper.h"
@@ -12,36 +11,32 @@ class Query_thd;
 
 class ycsb_request {
 public:
-    access_t rtype;
-    uint64_t key;
-    char value;
-    // only for (qtype == SCAN)
-    UInt32 scan_len;
+	access_t rtype;
+	uint64_t key;
+	char value;
+	// only for (qtype == SCAN)
+	UInt32 scan_len;
 };
 
 class ycsb_query : public base_query {
 public:
 
-    void init(uint64_t thd_id, workload * h_wl) {
-        assert(false);
-    };
-    void setbench_deinit();
-    void init(uint64_t thd_id, workload * h_wl, Query_thd * query_thd);
-    static void calculateDenom();
+	void init(uint64_t thd_id, workload * h_wl) { assert(false); };
+	void setbench_deinit();
+	void init(uint64_t thd_id, workload * h_wl, Query_thd * query_thd);
+	static void calculateDenom();
 
-    uint64_t request_cnt;
-    ycsb_request * requests;
+	uint64_t request_cnt;
+	ycsb_request * requests;
 
 private:
-    void gen_requests(uint64_t thd_id, workload * h_wl);
-    // for Zipfian distribution
-    static double zeta(uint64_t n, double theta);
-    uint64_t zipf(uint64_t n, double theta);
+	void gen_requests(uint64_t thd_id, workload * h_wl);
+	// for Zipfian distribution
+	static double zeta(uint64_t n, double theta);
+	uint64_t zipf(uint64_t n, double theta);
 
-    static uint64_t the_n;
-    static double denom;
-    double zeta_2_theta;
-    Query_thd * _query_thd;
+	static uint64_t the_n;
+	static double denom;
+	double zeta_2_theta;
+	Query_thd * _query_thd;
 };
-
-#endif
--- benchmarks/macrobench/benchmarks/ycsb_txn.cpp
+++ benchmarks/macrobench/benchmarks/ycsb_txn.cpp
@@ -15,77 +15,79 @@
 #include "mem_alloc.h"
 #include "query.h"
 
-void ycsb_txn_man::init(thread_t * h_thd, workload * h_wl, uint64_t thd_id) {
-    txn_man::init(h_thd, h_wl, thd_id);
-    _wl = (ycsb_wl *) h_wl;
+void ycsb_txn_man::init(thread_t * h_thd, workload * h_wl, uint64_t thd_id)
+{
+	txn_man::init(h_thd, h_wl, thd_id);
+	_wl = (ycsb_wl *) h_wl;
 }
 
-RC ycsb_txn_man::run_txn(base_query * query) {
-    RC rc;
-    ycsb_query * m_query = (ycsb_query *) query;
-    ycsb_wl * wl = (ycsb_wl *) h_wl;
-    itemid_t * m_item = NULL;
-    row_cnt = 0;
+RC ycsb_txn_man::run_txn(base_query * query)
+{
+	RC rc;
+	ycsb_query * m_query = (ycsb_query *) query;
+	ycsb_wl * wl = (ycsb_wl *) h_wl;
+	itemid_t * m_item = NULL;
+	row_cnt = 0;
 
-    for (uint32_t rid = 0; rid<m_query->request_cnt; rid++) {
-        ycsb_request * req = &m_query->requests[rid];
-        uint64_t key = req->key+1; //dirty hack to make sure key != 0	
-        int part_id = wl->key_to_part(key);
-        bool finish_req = false;
-        UInt32 iteration = 0;
-        while (!finish_req) {
-            if (iteration==0) {
-                m_item = index_read(_wl->the_index, key, part_id);
-                if (m_item==NULL) {
-                    cout<<"item in null, key is "<<key<<std::endl;
-                }
-                assert(m_item!=NULL);
-            }
+	for (uint32_t rid = 0; rid<m_query->request_cnt; rid++) {
+		ycsb_request * req = &m_query->requests[rid];
+		uint64_t key = req->key+1; //dirty hack to make sure key != 0	
+		int part_id = wl->key_to_part(key);
+		bool finish_req = false;
+		UInt32 iteration = 0;
+		while (!finish_req) {
+			if (iteration==0) {
+				m_item = index_read(_wl->the_index, key, part_id);
+				if (m_item==NULL) {
+					cout<<"item in null, key is "<<key<<std::endl;
+				}
+				assert(m_item!=NULL);
+			}
 //#ifdef IDX_BTREE
-//            else {
-//                _wl->the_index->index_next(get_thd_id(), m_item);
-//                if (m_item==NULL)
-//                    break;
-//            }
+//			else {
+//				_wl->the_index->index_next(get_thd_id(), m_item);
+//				if (m_item==NULL)
+//					break;
+//			}
 //#endif
-            row_t * row = ((row_t *) m_item->location);
-            row_t * row_local;
-            access_t type = req->rtype;
+			row_t * row = ((row_t *) m_item->location);
+			row_t * row_local;
+			access_t type = req->rtype;
 
-            row_local = get_row(row, type);
-            if (row_local==NULL) {
-                rc = Abort;
-                goto final;
-            }
+			row_local = get_row(row, type);
+			if (row_local==NULL) {
+				rc = Abort;
+				goto final;
+			}
 
-            // Computation //
-            // Only do computation when there are more than 1 requests.
-            if (m_query->request_cnt>1) {
-                if (req->rtype==RD||req->rtype==SCAN) {
-                    //                  for (int fid = 0; fid < schema->get_field_cnt(); fid++) {
-                    int fid = 0;
-                    char * data = row_local->get_data();
-                    __attribute__ ((unused)) uint64_t fval = *(uint64_t *) (&data[fid*10]);
-                    //                  }
-                } else {
-                    assert(req->rtype==WR);
-                    //					for (int fid = 0; fid < schema->get_field_cnt(); fid++) {
-                    int fid = 0;
-                    char * data = row->get_data();
-                    *(uint64_t *) (&data[fid*10]) = 0;
-                    //					}
-                }
-            }
+			// Computation //
+			// Only do computation when there are more than 1 requests.
+			if (m_query->request_cnt>1) {
+				if (req->rtype==RD||req->rtype==SCAN) {
+					//				  for (int fid = 0; fid < schema->get_field_cnt(); fid++) {
+					int fid = 0;
+					char * data = row_local->get_data();
+					__attribute__ ((unused)) uint64_t fval = *(uint64_t *) (&data[fid*10]);
+					//				  }
+				} else {
+					assert(req->rtype==WR);
+					//					for (int fid = 0; fid < schema->get_field_cnt(); fid++) {
+					int fid = 0;
+					char * data = row->get_data();
+					*(uint64_t *) (&data[fid*10]) = 0;
+					//					}
+				}
+			}
 
 
-            iteration++;
-            if (req->rtype==RD||req->rtype==WR||iteration==req->scan_len)
-                finish_req = true;
-        }
-    }
+			iteration++;
+			if (req->rtype==RD||req->rtype==WR||iteration==req->scan_len)
+				finish_req = true;
+		}
+	}
 
-    rc = RCOK;
+	rc = RCOK;
 final:
-    rc = finish(rc);
-    return rc;
+	rc = finish(rc);
+	return rc;
 }
